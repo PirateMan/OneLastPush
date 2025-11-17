830efe5d5afb573e53d5a0c23e84df9be1e43306
PirateMan <hciaran@gmail.com>
2025-11-17 11:22:48 +0000

    Starting Point

---
+++ OneLastPush/Source/OneLastPush/OneLastPush.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "OneLastPush.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, OneLastPush, "OneLastPush" );
+
+DEFINE_LOG_CATEGORY(LogOneLastPush)
+ 
